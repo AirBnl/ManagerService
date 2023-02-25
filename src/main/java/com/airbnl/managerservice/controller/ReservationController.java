9538d357b4fc30a7201271c62a51e860dafc6cfb
omar <omar10zoubi@gmail.com>
2023-02-25 13:55:42 +0300

    feat: implemented Reservation Controller #1

--- src/main/java/com/airbnl/managerservice/controller/ReservationController.java
+++ src/main/java/com/airbnl/managerservice/controller/ReservationController.java
@@ -1,13 +1,46 @@
 package com.airbnl.managerservice.controller;
 
+import com.airbnl.managerservice.model.Reservation;
 import com.airbnl.managerservice.service.Interfaces.IReservationService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/reservations")
 public class ReservationController {
     private final IReservationService reservationService;
 
     public ReservationController(IReservationService reservationService) {
         this.reservationService = reservationService;
+    }
+
+    @PutMapping
+    String save(Reservation reservation, int managerId, Model model) {
+        Reservation svedReservation = reservationService.save(reservation, managerId);
+
+        model.addAttribute("reservation" , svedReservation);
+        return "reservation";
+    }
+    @GetMapping(path = "/AllByHotelAndManagerId/{hotelId}/{managerId}")
+    String getAllByHotelAndManagerId(@PathVariable int hotelId
+            , @PathVariable int managerId, Model model){
+        List<Reservation> reservatinList = reservationService.getAllByHotelAndManagerId(hotelId, managerId);
+
+        model.addAttribute("reservationList" , reservatinList);
+        return "reservations";
+    }
+    @GetMapping(path = "/ByReservationIdAndManagerId/{reservationId}/{managerId}")
+    String getByReservationIdAndManagerId(@PathVariable int reservationId
+            , @PathVariable int managerId, Model model){
+        Reservation reservation = reservationService.getByReservationIdAndManagerId(reservationId, managerId);
+
+        model.addAttribute("reservation" , reservation);
+        return "reservation";
     }
 }
