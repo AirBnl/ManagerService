578ba8a4b1b4b0d4f43f77877a7a4779d4b18a0c
omar <omar10zoubi@gmail.com>
2023-03-11 21:11:22 +0300

    fix: get id from session

--- src/main/java/com/airbnl/managerservice/controller/HotelController.java
+++ src/main/java/com/airbnl/managerservice/controller/HotelController.java
@@ -2,6 +2,8 @@ package com.airbnl.managerservice.controller;
 
 import com.airbnl.managerservice.model.Hotel;
 import com.airbnl.managerservice.service.Interfaces.IHotelService;
+import com.airbnl.managerservice.service.Interfaces.IUserService;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -13,34 +15,40 @@ import java.util.List;
 @RequestMapping("/hotels")
 public class HotelController {
     private final IHotelService hotelService;
-
-    public HotelController(IHotelService hotelService) {
+    private final IUserService userService;
+    public HotelController(IHotelService hotelService, IUserService userService) {
         this.hotelService = hotelService;
+        this.userService = userService;
     }
 
     @PostMapping
-    public String save(Hotel hotel, @RequestParam long managerId, Model model) {
+    public String save(Hotel hotel, Model model) {
         Hotel SavedHotel = hotelService.save(hotel);
 
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
         model.addAttribute("hotel", SavedHotel);
-        model.addAttribute("managerId", managerId);
 
         return "hotel";
     }
 
     @GetMapping(path = "/AllByManagerID")
     public String getAllByManagerID( Model model) {
-//        List<Hotel> hotels = hotelService.getAllByManagerID(managerId);
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        long managerId = userService.getByUserName(username, "").getId();
 
-//        model.addAttribute("hotels", hotels);
-//        model.addAttribute("managerId", managerId);
+        List<Hotel> hotels = hotelService.getAllByManagerID(managerId);
+
+        model.addAttribute("hotels", hotels);
 
         return "hotels";
     }
 
     @GetMapping(path = "/ByHotelIdAndManagerId")
-    public String getByHotelIdAndManagerId(@RequestParam long hotelId
-            , @RequestParam long managerId, Model model) {
+    public String getByHotelIdAndManagerId(@RequestParam long hotelId, Model model) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        long managerId = userService.getByUserName(username, "").getId();
+
         Hotel hotel = hotelService.getByHotelIdAndManagerId(hotelId, managerId);
 
         model.addAttribute("hotel", hotel);
@@ -50,22 +58,23 @@ public class HotelController {
     }
 
     @PutMapping
-    public String update(@RequestBody Hotel hotel, @RequestParam long managerId
-            , Model model) {
-        // todo save manager id in hotel
+    public String update(@RequestBody Hotel hotel, Model model) {
         Hotel updatedHotel = hotelService.update(hotel);
 
         model.addAttribute("hotel", updatedHotel);
-        model.addAttribute("managerId", managerId);
 
         return "hotel";
     }
 
     @DeleteMapping(path = "/ByIdAndManagerID")
-    public String deleteByIdAndManagerID(@RequestParam long hotelId
-            , @RequestParam long managerId, Model model) {
+    public String deleteByIdAndManagerID(@RequestParam long hotelId, Model model) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        long managerId = userService.getByUserName(username, "").getId();
+
         Hotel deletedHotel = hotelService.deleteByIdAndManagerID(hotelId, managerId);
+
         model.addAttribute("hotel", deletedHotel);
+
         return "hotel";
     }
 }
--- src/main/java/com/airbnl/managerservice/controller/ReservationController.java
+++ src/main/java/com/airbnl/managerservice/controller/ReservationController.java
@@ -2,6 +2,8 @@ package com.airbnl.managerservice.controller;
 
 import com.airbnl.managerservice.model.Reservation;
 import com.airbnl.managerservice.service.Interfaces.IReservationService;
+import com.airbnl.managerservice.service.Interfaces.IUserService;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -12,9 +14,11 @@ import java.util.List;
 @RequestMapping("/reservations")
 public class ReservationController {
     private final IReservationService reservationService;
+    private final IUserService userService;
 
-    public ReservationController(IReservationService reservationService) {
+    public ReservationController(IReservationService reservationService, IUserService userService) {
         this.reservationService = reservationService;
+        this.userService = userService;
     }
 
     @PutMapping
@@ -27,22 +31,24 @@ public class ReservationController {
         return "reservation";
     }
     @GetMapping(path = "/AllByHotelAndManagerId")
-    String getAllByHotelAndManagerId(@RequestParam Long hotelId
-            , @RequestParam Long managerId, Model model){
+    String getAllByHotelAndManagerId(@RequestParam Long hotelId, Model model){
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        long managerId = userService.getByUserName(username, "").getId();
+
         List<Reservation> reservatinList = reservationService.getAllByHotelAndManagerId(hotelId, managerId);
 
         model.addAttribute("reservationList" , reservatinList);
-        model.addAttribute("managerId" , managerId);
 
         return "reservations";
     }
     @GetMapping(path = "/ByReservationIdAndManagerId")
-    String getByReservationIdAndManagerId(@RequestParam Long reservationId
-            , @RequestParam Long managerId, Model model){
+    String getByReservationIdAndManagerId(@RequestParam Long reservationId, Model model){
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        long managerId = userService.getByUserName(username, "").getId();
+
         Reservation reservation = reservationService.getByReservationIdAndManagerId(reservationId, managerId);
 
         model.addAttribute("reservation" , reservation);
-        model.addAttribute("managerId" , managerId);
 
         return "reservation";
     }
