e335491a174d541e399523fd40ebe82a9108bd1f
omar <omar10zoubi@gmail.com>
2023-03-12 00:00:08 +0300

    feat: added country

---
+++ src/main/java/com/airbnl/managerservice/service/Interfaces/ICountryService.java
@@ -0,0 +1,10 @@
+package com.airbnl.managerservice.service.Interfaces;
+
+import com.airbnl.managerservice.model.Country;
+
+import java.util.List;
+
+public interface ICountryService {
+    List<Country> getAll();
+    Country save(Country country);
+}
